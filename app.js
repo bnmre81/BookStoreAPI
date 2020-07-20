ec68e74c0d6c478918467d06fde82b4bf9aaafd2
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-07-20 19:39:22 +0300

    create

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 let books = require("./books");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   console.log("HELLO");
@@ -24,6 +26,14 @@ app.delete("/books/:bookId", (req, res) => {
   } else {
     res.status(404).json({ message: "book not found" });
   }
+});
+
+app.post("/books", (req, res) => {
+  console.log(req);
+  const id = books[books.length - 1].id + 1;
+  const newBook = { ...req.body, id }; // id is equivalent to id: id
+  books.push(newBook);
+  res.status(201).json(newBook);
 });
 
 app.listen(8000, () => {
