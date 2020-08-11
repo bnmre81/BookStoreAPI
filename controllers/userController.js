accd3857379455caa40bc84e439703c69061bc4f
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-08-11 20:13:22 +0300

    newuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,14 +4,12 @@ const bcrypt = require("bcrypt");
 const { User } = require("../db/models");
 
 exports.signup = async (req, res, next) => {
-  const { password } = req.body;
-  const saltRounds = 10;
   try {
+    const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    console.log("exports.signup -> hashedPassword", hashedPassword);
     req.body.password = hashedPassword;
     const newUser = await User.create(req.body);
-    res.json({ message: "User created successfully" });
+    res.status(201).json(newUser);
   } catch (error) {
     next(error);
   }
